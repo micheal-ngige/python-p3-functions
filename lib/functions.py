e4624f0436bd7142dba290472fe3ef095345ee01
michael <michaelngige@gmail.com>
2023-11-20 21:21:05 +0300

    tests completed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print( "Hello, programmer!" )
 
 def greet(name):
-    pass
+    print("Hello, " + name + "!")
+
+greet("Guido")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+    
+    
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    result = num1 + num2
+    print(result)
+    return result
+
+
+sum_result = add(45, 55)
+
+
+print(f"The sum is: {sum_result}")
+  
 
 def halve(number):
-    pass
+    result = number / 2
+    print(result)
+    return result
+
+
+halved_value = halve(100)
+
+
+print(f"The halved value is: {halved_value}")
+
+
